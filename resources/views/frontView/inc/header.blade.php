8356afa715635b9e2919bb3ecc68d2517d2b696d
Saima98 <saimanajmin750@gmail.com>
2019-03-12 22:19:38 +0600

    header area updated

--- resources/views/frontView/inc/header.blade.php
+++ resources/views/frontView/inc/header.blade.php
@@ -1,21 +1,27 @@
-<div class="header">
+	<div class="header">
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-2">
+				<div class="col-lg-2">
 					<div class="logo">
-						<a href="#home"><span>Rokto</span>Man</a>
+						<a href="#home"><span>rokto</span>man</a>
 					</div>
 				</div>
-				<div class="col-10">
+				<div class="col-lg-10">
 					<div id="navbar" class="content">
-						<ul class="nav">
-							<li class="current"><a href="#home">Home</a></li>
-							<li class="current"><a href="#why_give">Why Give Blood</a></li>
-							<li class="current"><a href="#who_can_give">Who Can Give Blood</a></li>
-							<li class="current"><a href="#achievement">Achievement</a></li>
-							<li class="current"><a href="#contact">Contact</a></li>
-							<li><a href="#" class="link">Sign In <i class="fas fa-portrait"></i></a></li>
-						</ul>
+						<div class="row">
+							<div class="menu">
+								<ul class="nav">
+									<li class="current"><a href="#home">home</a></li>
+									<li class="current"><a href="#why_give">why give blood</a></li>
+									<li class="current"><a href="#who_can_give">who can give blood</a></li>
+									<li class="current"><a href="#achievement">achievement</a></li>
+									<li class="current"><a href="#contact">contact</a></li>
+								</ul>
+							</div>
+							<div class="menu_link">
+								<a href="#" class="link">sign in <i class="fas fa-portrait"></i></a>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
