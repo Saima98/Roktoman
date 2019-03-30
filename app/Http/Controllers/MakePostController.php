f5e67cac9dae540ca9890972796c4037cd1e3756
Rafat001 <rafatislam.syl@gmail.com>
2019-03-30 18:35:12 +0600

    adminship logged in

---
+++ app/Http/Controllers/MakePostController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class MakePostController extends Controller
+{
+    public function makepost(){
+		return view('frontView.home.makepost');
+	}
+}
