e49eca6930e75313b9e11b3c84a874c9daa9a7a2
Majharul Islam Rafat <rafatislam.syl@gmail.com>
2019-03-30 18:46:44 +0600

    Update Bloodrequest.php

--- app/Bloodrequest.php
+++ app/Bloodrequest.php
@@ -1,13 +1,4 @@
 <?php
-<<<<<<< HEAD
-namespace App;
-use Illuminate\Database\Eloquent\Model;
-class Bloodrequest extends Model
-{
-    //
-}
-=======
-
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
@@ -16,4 +7,3 @@ class Bloodrequest extends Model
 {
     //
 }
->>>>>>> 8abad6be6d525db83a4f42df299f8cff192d3e38
